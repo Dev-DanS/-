97e582a2ff4c78a018d0e91930dc9ec84a765839
Dan <146542914+Dev-DanS@users.noreply.github.com>
2024-03-03 12:07:08 +0800

    Add files via upload

---
+++ commuter/commuter.php
@@ -0,0 +1,19 @@
+<?php
+session_start(); // Start the session
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>ID: <?php echo $_SESSION["commuterid"]; ?></h1>
+    <h1>Role: <?php echo $_SESSION["role"]; ?>
+    </h1>
+</body>
+
+</html>
